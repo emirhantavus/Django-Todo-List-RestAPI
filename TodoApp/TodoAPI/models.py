bf4dedd732d77ff77918bf210b67ea6a4b553a90
Emirhan Tavus <Whisber1@gmail.com>
2023-09-12 22:50:58 +0300

    Task model created

--- TodoApp/TodoAPI/models.py
+++ TodoApp/TodoAPI/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from datetime import datetime
+
+class Task(models.Model):
+      title = models.CharField(max_length=255)
+      description = models.TextField(null=True, blank=True)
+      completed = models.BooleanField(default=False)
+      created_at = models.DateTimeField(auto_now=True)
+      user = models.CharField(max_length=50)
+
+      def __str__(self):
+            return self.title
